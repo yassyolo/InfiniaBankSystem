e266bf5e9e8a03b5feb812b70d2022b7651c694e
yassyolo <yoana.yotova03@gmail.com>
2024-09-09 19:26:49 +0300

    Initial commit

---
+++ Infinia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Infinia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
